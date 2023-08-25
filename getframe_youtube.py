4c4a62d87f1b31d31b91cab145a6e18a87826a19
haeuncreate <haeun991222@gmail.com>
2023-08-25 13:45:11 +0900

    using Excel file containing video information

--- getframe_youtube.py
+++ getframe_youtube.py
@@ -1,108 +1,100 @@
 from pytube import YouTube
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import numpy as np
-import subprocess
-import shlex
 import cv2
 import os
 import re
+import pandas as pd
+from datetime import datetime, timedelta
 
-# 유튜브 url(영상) 하나에 대해 생기는 클래스
-class Youtube_to_image:
-    def __init__(self, ss_array, output_path, url):
-        self.ss_array = ss_array
-        self.output_path = output_path
+# Input Configuration
+output_video_folder_path = "./video"
+output_image_folder_path = "./image"
+excel_file_path = "./data_youtube_hanging.xlsx" # Path to the Excel file containing video information
+frame_interval = 3
+
+class YouTubeToImage:
+    def __init__(self, video_time_segments, output_video_folder_path, output_image_folder_path, frame_interval, url):
+        self.video_time_segments = video_time_segments
+        self.output_video_folder_path = output_video_folder_path  
+        self.output_image_folder_path = output_image_folder_path
+        self.frame_interval = frame_interval
         self.url = url
 
-    def youtube_download(self):
-        download_folder = os.path.join(self.output_path, 'video')
+        # Ensure the output folders exist or create them
+        os.makedirs(self.output_video_folder_path, exist_ok=True)
+        os.makedirs(self.output_image_folder_path, exist_ok=True)
 
-        new_file = os.path.join(download_folder, self.url) + '.mp4'
-        # 이미 영상이 존재하는 경우
-        if os.path.exists(new_file):
-            print(self.url + ' video file already exists')
-            return new_file
+    def download_video(self):
+        video_path_by_url = os.path.join(self.output_video_folder_path, f"{self.url}.mp4")
 
-        yt = YouTube('https://www.youtube.com/watch?v=' + self.url, use_oauth=True, allow_oauth_cache=True)
+        if os.path.exists(video_path_by_url):
+            return video_path_by_url
+
+        yt = YouTube(f'https://www.youtube.com/watch?v={self.url}', use_oauth=True, allow_oauth_cache=True)
 
         try:
-            video_name = yt.title
-
-            # 영상 title 로컬에 저장되는 형식으로 변경
-            video_name = re.sub(r'[#:;.,\<"|/>*?]', '', video_name)
-            video_name = re.sub(r"[']", '', video_name)
+            video_name = re.sub(r'[#:;.,\<"|/>*?\']', '', yt.title)
             stream = yt.streams.filter(file_extension='mp4').get_highest_resolution()
-            stream.download(download_folder)
-
-            # 영상 title url로 형식으로 변경
-            old_file = os.path.join(download_folder, video_name) + '.mp4'
-            os.rename(old_file, new_file)
+            stream.download(self.output_video_folder_path)
+            os.rename(os.path.join(self.output_video_folder_path, f"{video_name}.mp4"), video_path_by_url)
 
         except Exception as e:
             print(e)
-            return 0
+            return None
         
+        print(f'{self.url} video download: success')
+        return video_path_by_url
 
-        video_path = new_file
-        print(self.url + ' video download : success')
-        return video_path
-
-
-    def oneframe_per_sec(self, input_path):
-        video = cv2.VideoCapture(input_path)
-        self.fps = video.get(cv2.CAP_PROP_FPS)
-        self.input_path = input_path
-        for idx, time in enumerate(self.ss_array):
-            s_time, e_time = time
-            if s_time == '':
-                return
-            command = f"ffmpeg -vsync 2 -ss {s_time} -to {e_time} -i '{self.input_path}' -vf thumbnail={self.fps} '{self.output_path}/image/O_{self.url}{idx+1}_%06d.jpg'"
-            process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            process.communicate()
-
-            if process.returncode == 0:
-                print(self.url + ' image download : success')
-            else:
-                print(self.url + ' image download : failed')
+    def extract_frames(self, video_file_path):
+        self.video = cv2.VideoCapture(video_file_path)
+        if not self.video.isOpened():
+            print("Could not open video file")
+            return
         
+        for start_time, end_time in self.video_time_segments:
+            if not start_time or not end_time:
+                continue
+            # Convert start_time from MM:SS format to datetime and add/subtract 1 second to exclude the frame boundary
+            start_time = self.parse_time_stamp(start_time) + timedelta(seconds=1)
+            end_time = self.parse_time_stamp(end_time) - timedelta(seconds=1)
+            while start_time <= end_time:
+                formatted_time = start_time.strftime("%M%S")
+                image_name = f"YT_{self.url}_{formatted_time}.png" 
+                start_time_sec = timedelta(minutes=start_time.minute, seconds=start_time.second).total_seconds()
+                self.capture_image(start_time_sec, image_name)
+                start_time += timedelta(seconds=self.frame_interval)
+
+        self.video.release()
+        cv2.destroyAllWindows()
+    
+    def capture_image(self, sec, image_name):
+        self.video.set(cv2.CAP_PROP_POS_MSEC, sec*1000)
+        ret, frame = self.video.read()
+
+        if ret:
+            image_path = os.path.join(self.output_image_folder_path, image_name)
+            cv2.imwrite(image_path, frame)
+
+    def parse_time_stamp(self, time_stamp):
+        return datetime.strptime(time_stamp, "%M:%S")
 
 def main():
-    # 추출된 이미지를 담을 output path 설정
-    output_path = "C:/Users/Snucem_W1/Desktop/YoutubeToImage"
-    
-    # 구글 스프레드시트 연동
-    scope = [
-    'https://spreadsheets.google.com/feeds',
-    'https://www.googleapis.com/auth/drive',
-    ]
-    json_file_name = 'sheetapi.json'
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
-    gc = gspread.authorize(credentials)
-    spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1oBu9HhFP1oQyHJTc-re3B7b56_fB-FEcA_cG3oW9Cng/edit?pli=1#gid=58112834'
-    # 스프레스시트 문서 가져오기 
-    doc = gc.open_by_url(spreadsheet_url)
-
-    worksheet = doc.worksheet('Youtube')
-    # B, C, D열에 해당하는 정보만 가져오기
-    array = np.array(worksheet.get_all_values()[2:])[:, 1:4]
-
-    url_list = list(set(array[:, 0]))
-    url_list.remove('')
-    print('number of videos in table :', len(url_list))
+    # Load Excel file containing video information
+    df = pd.read_excel(excel_file_path, sheet_name='Sheet1')  
+    url_list = df['Youtube ID'].unique()
+    print('Number of videos in table:', len(url_list))
 
     for url in url_list:
-        # 해당 url에 대응하는 start, stop 표를 가져오기
-        ss_array = array[array[:, 0] == url][:, 1:3]
-        yt = Youtube_to_image(ss_array, output_path, url)
-        video_path = yt.youtube_download()
-        if video_path == 0:
+        # Get time segments (Start time and End time) for the current video
+        video_time_segments = df[df['Youtube ID'] == url][['Start time', 'End time']].values
+        yt = YouTubeToImage(video_time_segments, output_video_folder_path, output_image_folder_path, frame_interval, url)
+
+        # Download the video if not already downloaded and extract frames
+        downloaded_video_path = yt.download_video()
+        if downloaded_video_path is None:
             continue
-        yt.oneframe_per_sec(video_path)
-        print(url + ' complete')
-    
+
+        yt.extract_frames(downloaded_video_path)
+        print(f'{url} image download: complete')
 
 if __name__ == "__main__":
     main()
-
-
